da03f1638852e1258c28ccb9f81becf3f811250d
Ihar Trymailau <ihar.trymailau@gmail.com>
2020-07-21 16:26:11 +0300

    add: getUsers

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { getUsers } from '../Service/Service';
+import { Table } from 'antd';
 
-const Users = () => {
-    return (
-        <div>
-            <span>Users</span>
-        </div>
-    )
+const {Column} = Table;
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            users: []
+        }
+    }
+    componentWillMount() {
+        getUsers('https://redevcrm.herokuapp.com/users')
+        .then(users => {
+            this.setState({
+                users,
+                isLoaded: true
+            })
+        })
+    }
+
+    render() {
+        console.log(this.state.users);
+        const {isLoaded, users} = this.state;
+        if(isLoaded) {
+            return (
+                // <div>
+                //     {users.map(user => (
+                //         <div key={user._id}>
+                //             First Name: {user.firstName}, Email: {user.email}, Birthday: {user.birthday}, Surname: {user.lastName}
+                //         </div>
+                //     ))}
+                // </div>
+                <Table dataSource={users}>
+                    <Column title='First Name' dataIndex='firstName' key='_id'/>
+                    <Column title='Email' dataIndex='email' key='_id'/>
+                    <Column title='Birthday' dataIndex='birthday' key='_id'/>
+                    <Column title='Surname' dataIndex='lastName' key='_id'/>
+                </Table>
+            )
+        }
+
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+        
+    }
+    
 }
 
 export default Users;

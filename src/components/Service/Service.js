34d732b320c177ab9d4b2abff579f4efa4c8e497
Ihar Trymailau <ihar.trymailau@gmail.com>
2020-08-05 11:47:08 +0300

    add: task get,post

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,87 +1,21 @@
 import axios from 'axios';
-// const getUsers = async (url) => {
-//     const res = await fetch(url);
 
-//     if(!res.ok) {
-//         throw new Error();
-//     }
-
-//     return res.json();
-// }
-
-
-
-// const getLeeds = async (url) => {
-//     const res = await fetch(url);
-
-//     if(!res.ok) {
-//         throw new Error(res.status);
-//     }
-
-//     return res.json();
-// }
-
-// const loginUser = async (url, data) => {
-//     const res = await fetch(url, {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     });
-
-//     if(!res.ok) {
-//         throw new Error(res.status);
-//     }
-
-//     return res.text();
-// }
-
-// const getQuotes = async (url) => {
-//     const res = await fetch(url);
-//     if(!res.ok) {
-//         throw new Error(res.status);
-//     }
-
-//     return res.json();
-// }
-
-// const postQuote = async (url, data) => {
-//     const res = await fetch(url, {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)  
-//     });
-
-//     if(!res.ok) {
-//         throw new Error(res.status);
-//     }
-
-//     return res.json();
-// }
-
-// const deleteQuote = async (item, url) => {
-//     const res = await fetch(url + '/' + item, {
-//         method: 'DELETE'
-//     });
-
-//     return res.json();
-// }
-
+//users
 const getUsers = async () => {
     return await axios.get('https://redevcrm.herokuapp.com/users').then(res => res.data);
 }
 
+//leeds
 const getLeeds = async () => {
     return await axios.get('https://redevcrm.herokuapp.com/leeds').then(res => res.data);
 }
 
+//login
 const loginUser = async (values) => {
     return await axios.post('https://redevcrm.herokuapp.com/users/login', values).then(res => res.data);
 }
 
+// quotes
 const getQuotes = async () => {
     return await axios.get('https://redevcrm.herokuapp.com/quotes').then(res => res.data.map(item => {
         const {_id, author, text} = item;
@@ -92,7 +26,6 @@ const getQuotes = async () => {
         }
     }));
 }
-
 const postQuote = async (value) => {
     return await axios.post('https://redevcrm.herokuapp.com/quotes', value).then(res => {
         const {_id, author, text} = res.data;
@@ -103,9 +36,30 @@ const postQuote = async (value) => {
                 }
     })
 }
-
 const deleteQuote = async (item) => {
     return axios.delete('https://redevcrm.herokuapp.com/quotes/' + item).then(res => res.data);
+}
+
+//tasks
+const getTasks = async () => {
+    return await axios.get('https://redevcrm.herokuapp.com/tasks').then(res => res.data.map(task => {
+        const {_id, theme, text} = task;
+        return {
+            key: _id,
+            theme,
+            text
+        }
+    }));    
+}
+const postTask = async (value) => {
+    return await axios.post('https://redevcrm.herokuapp.com/tasks', value).then(res => {
+        const {_id, theme, text} = res.data;
+        return {
+            key: _id,
+            theme,
+            text
+        }
+    });
 }
 
 export {
@@ -114,5 +68,7 @@ export {
     loginUser, 
     getQuotes, 
     postQuote,
-    deleteQuote
+    deleteQuote,
+    getTasks,
+    postTask
 };
--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,11 +1,139 @@
 import React from 'react';
+import {getTasks, postTask} from '../Service/Service';
+import {Table, Button, Modal, Form, Input} from 'antd';
 
-const Tasks = () => {
-    return (
-        <div>
-            <span>Tasks</span>
-        </div>
-    )
+class Tasks extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            columns: [
+                {
+                    title: 'Theme',
+                    dataIndex: 'theme',
+                    key: 'theme',
+                },
+                {
+                    title: 'Text',
+                    dataIndex: 'text',
+                    key: 'text',
+                }
+            ],
+            visible: false,
+            errorMessage: ''
+        }
+    }
+
+    showForm = () => {
+        this.setState({visible: true})
+    }
+
+    handleOk = () => {
+        this.setState({
+            visible: false,
+            errorMessage: ''
+        });
+    }
+    
+    handleCancel = () => {
+        this.setState({ 
+            visible: false,
+            errorMessage: '' 
+        });
+    }
+
+    onFinish = (value) => {
+        postTask(value)
+            .then(task => this.setState({
+                data: [...this.state.data, {...task}],
+                errorMessage: '',
+                visible: false
+            }))
+            .catch(error => error ? this.setState({
+                    errorMessage: 'Something went wrong, check the entered data'
+                }) : null
+            );
+    }
+
+    componentDidMount() {
+        getTasks().then(res => this.setState({data: res}));
+    }
+
+    render() {
+
+        const {data, columns} = this.state;
+
+        const layout = {
+            labelCol: {span: 8},
+            wrapperCol: {span: 12}
+        }
+
+        const tailLayout = {
+            wrapperCol: { offset: 0, span: 4 },
+        }
+
+        return (
+            <div>
+                <Button 
+                    type="primary"
+                    onClick={this.showForm}
+                    style={{
+                        margin: '16px'
+                    }}
+                >
+                    Create task
+                </Button>
+                <Table 
+                    dataSource={data} 
+                    columns={columns}
+                    style={{
+                        margin: '0 16px'
+                    }}
+                />
+
+                <Modal 
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                >
+
+                    <Form
+                        {...layout}
+                        onFinish={this.onFinish}
+                    >
+                        <Form.Item 
+                            label='Theme'
+                            name='theme'
+                            rules={[{ required: true, message: 'Please input theme! '}]}
+                        >
+                            <Input/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label='Text'
+                            name='text'
+                            rules={[{ required: true, message: 'Please input your text! '}]}
+                        >
+                            <Input/>
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout}>
+                            <Button  
+                                type='primary' 
+                                htmlType='submit'
+                            >
+                                Add task
+                            </Button>
+                            
+                        </Form.Item>
+                    </Form>
+                    <span>{this.state.errorMessage ? this.state.errorMessage : null}</span>
+                </Modal>
+            </div>
+        )
+    }
 }
 
 export default Tasks;
